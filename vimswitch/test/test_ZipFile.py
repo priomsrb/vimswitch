1f7fc152bd32c63b3e0f68297d6e8d6967556f6d
Shafqat Bhuiyan <priomsrb@gmail.com>
2014-09-09 19:44:42 +1200

    Added some tests for unzipping files

---
+++ vimswitch/test/test_ZipFile.py
@@ -0,0 +1,28 @@
+from FileSystemTestCase import FileSystemTestCase
+from vimswitch.DiskIo import DiskIo
+from zipfile import ZipFile
+
+
+class TestZipFile(FileSystemTestCase):
+    def setUp(self):
+        super(TestZipFile, self).setUp()
+        self.diskIo = DiskIo()
+
+    def tearDown(self):
+        super(TestZipFile, self).tearDown()
+
+    def test_extract_extractsFile(self):
+        zipPath = self.getDataPath('simple.zip')
+        destPath = self.getTestPath('')
+        destFile1Path = self.getTestPath('file1.txt')
+        destFile2Path = self.getTestPath('file2.txt')
+        zipFile = ZipFile(zipPath)
+
+        zipFile.extractall(destPath)
+
+        file1Expected = 'test data'
+        file1Actual = self.diskIo.getFileContents(destFile1Path)
+        file2Expected = 'test data 2'
+        file2Actual = self.diskIo.getFileContents(destFile2Path)
+        self.assertTrue(file1Actual, file1Expected)
+        self.assertTrue(file2Actual, file2Expected)
