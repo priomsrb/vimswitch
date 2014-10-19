5013d0aba5cc9fcf5bb7e9d1dab4fcd0dc35d58b
Shafqat Bhuiyan <priomsrb@gmail.com>
2014-10-20 03:16:17 +1300

    Removed fake DiskIo classes
    Right now I prefer using the real DiskIo class as it's a better representation of real disk operations on different operating systems.
    
    If DiskIo becomes too slow in the future, I may bring back one of the fakes to use for specific tests.

--- vimswitch/test/test_FakeFsDiskIo.py
+++
@@ -1,8 +0,0 @@
-from .BaseTestCase import BaseTestCase
-from .CommonDiskIoTests import CommonDiskIoTests
-from .FakeFsDiskIo import FakeFsDiskIo
-
-
-class TestFakeFsDiskIo(BaseTestCase, CommonDiskIoTests):
-    def setUp(self):
-        self.diskIo = FakeFsDiskIo()
