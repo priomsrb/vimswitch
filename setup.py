82798238357c2337fd402dc74f15a449d28a4e8c
Shafqat Bhuiyan <priomsrb@gmail.com>
2014-10-20 03:21:47 +1300

    Added tox settings
    Also updated setup.py with some preliminary values

--- setup.py
+++ setup.py
@@ -1,2 +1,19 @@
 # Built using python 2.7
 # Dependencies: nose, mock
+
+from distutils.core import setup
+
+setup(
+    name='vimswitch',
+    version='0.1',
+    description='A utility for switching vim profiles',
+    author='Shafqat Bhuiyan',
+    author_email='priomsrb@gmail.com',
+    url='https://github.com/priomsrb/vimswitch',
+    packages=['vimswitch', 'vimswitch.test'],
+    package_data={'vimswitch': ['vimswitch/*'], 'vimswitch.test': ['vimswitch/test/*']}
+    #long_description='''Really long text here.'''
+    #
+    #This next part it for the Cheese Shop
+    #classifiers=[]
+)
